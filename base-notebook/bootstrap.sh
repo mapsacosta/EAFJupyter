8420a8cd77771f4d3bd9c9336cd321cfab152074
macosta <macosta@fnal.gov>
2021-06-16 09:28:31 -0500

    Verbose logging for bootstrap script

--- base-notebook/bootstrap.sh
+++ base-notebook/bootstrap.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-echo Boootstrapping this thing...
-echo Gathering parameters
-echo Username $1
-echo UID $2 
-echo GID $3
-
-mkdir -p /uscmst1b_scratch/lpc1
-mkdir -p /uscms_data/d1
-mkdir -p /uscms_data/d2
-mkdir -p /uscms_data/d3
-mkdir -p /uscms
-#mount --internal-only --no-mtab -t nfs cmsnfs1.fnal.gov:/uscms_data/scratch1 /uscmst1b_scratch/lpc1
-
+echo ===== Bootstrapping
+echo This Jupyter notebook was requested by:
+echo Username $NB_USER
+echo UID $NB_UID
+echo GID $NB_GID
