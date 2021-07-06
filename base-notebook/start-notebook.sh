3ae06a5aa275cc1f500a10581bf0d398d6c5da39
macosta <macosta@fnal.gov>
2021-07-06 13:49:58 -0500

    Removing hardcoded config from base image

--- base-notebook/start-notebook.sh
+++ base-notebook/start-notebook.sh
@@ -14,7 +14,7 @@ fi
 
 if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
     # launched by JupyterHub, use single-user entrypoint
-     exec /usr/local/bin/start-singleuser.sh  --SingleUserNotebookApp.default_url=/lab --SingleUserNotebookApp.disable_user_config=True --SingleUserNotebookApp.notebook_dir="/home/$NB_USER" "$@"
+     exec /usr/local/bin/start-singleuser.sh "$@"
 elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
     . /usr/local/bin/start.sh $wrapper jupyter lab "$@"
 else
