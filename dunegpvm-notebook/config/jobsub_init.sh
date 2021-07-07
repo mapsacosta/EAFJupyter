9b6a4c50c8e528cc26ab94834166736544e4c806
macosta <macosta@fnal.gov>
2021-07-07 17:14:09 -0500

    Incorporating init.sh behavior

--- dunegpvm-notebook/config/jobsub_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setup
-source /cvmfs/oasis.opensciencegrid.org/fermilab/products/common/etc/setup
-#This establishes a UPS product working area (more about this later)
-setup jobsub_client
-setup ifdhc
-printenv
