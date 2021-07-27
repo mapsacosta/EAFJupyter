1c003d41a94887ca780df7ead47dbdb568031ae1
macosta <macosta@fnal.gov>
2021-07-27 12:50:23 -0500

    Installing krb5 workstation on base

--- base-notebook/init.sh
+++ base-notebook/init.sh
@@ -2,6 +2,9 @@
 
 echo ===== Bootstrapping
 echo This Jupyter notebook was requested by:
-echo Username $NB_USER
-echo UID $NB_UID
-echo GID $NB_GID
+echo * Username $NB_USER
+echo * UID $NB_UID
+echo * GID $NB_GID
+
+echo Initializing krb5 configuration
+wget https://authentication.fnal.gov/krb5conf/SL7/krb5.conf --output-document=/etc/krb5.conf
