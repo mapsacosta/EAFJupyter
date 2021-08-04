48a032386ee5777561802f816e12c1e9e32fdc5a
macosta <macosta@fnal.gov>
2021-08-04 13:59:18 -0500

    Fixing existing directory error

--- accel-ai-notebook/config/init.sh
+++ accel-ai-notebook/config/init.sh
@@ -1,5 +1,9 @@
 # Initializing READS repository
-git clone git@github.com:fermilab-accelerator-ai/READS.git /opt/
+cd /home/$NB_USER
+git clone git@github.com:fermilab-accelerator-ai/READS.git
+
+# Fix permissions
+chown -R $NB_USER:$NB_GROUP /home/$NB_USER
 
 # Creating READS unix group and adding $NB_USER to it
 groupadd -g 1005 READS
@@ -7,5 +11,4 @@ usermod -a -g READS ${NB_USER}
 
 # Preparating mount subdirectories
 #mkdir /store/READS/
-#chmod g+rwx foldername 
-
+#chmod g+rwx /store/READS
