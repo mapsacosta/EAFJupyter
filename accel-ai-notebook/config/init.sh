405f71c730ac8ba20540aaed591d70dda953b082
macosta <macosta@fnal.gov>
2021-08-04 14:45:47 -0500

    Leaving init file intact

--- accel-ai-notebook/config/init.sh
+++ accel-ai-notebook/config/init.sh
@@ -1,13 +1,13 @@
 # Initializing READS repository
-cd /home/$NB_USER
-git clone git@github.com:fermilab-accelerator-ai/READS.git
+#cd /home/$NB_USER
+#git clone git@github.com:fermilab-accelerator-ai/READS.git
 
 # Fix permissions
-chown -R $NB_USER:$NB_GROUP /home/$NB_USER
+#chown -R $NB_USER:$NB_GROUP /home/$NB_USER
 
 # Creating READS unix group and adding $NB_USER to it
-groupadd -g 1005 READS
-usermod -a -g READS ${NB_USER}
+#groupadd -g 1005 READS
+#usermod -a -g READS ${NB_USER}
 
 # Preparating mount subdirectories
 #mkdir /store/READS/
