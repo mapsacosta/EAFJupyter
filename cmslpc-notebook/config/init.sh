2e964525d9071a3dfe37ba13eb83fe59abc6044e
macosta <macosta@fnal.gov>
2021-08-13 16:52:09 -0500

    Adding new wrappers for condor_submit

--- cmslpc-notebook/config/init.sh
+++ cmslpc-notebook/config/init.sh
@@ -12,12 +12,12 @@ declare -a REPLACE_CONDOR_RM_LINKS=('condor_vacate_job' 'condor_suspend' 'condor
 declare -a DELETE_CONDOR_RM_LINKS=('condor_hold' 'condor_release')
 declare -a NAME_FLAG_CHK_CMD=('condor_rm' 'condor_tail' 'condor_qedit' 'condor_hold' 'condor_release''condor_history')
 
-git clone https://hepcloud-git.fnal.gov/ECF-GCO-public/gco_scripts.git /tmp/gco_scripts
+git clone -q -b fkhan_notebook_interactives --single-branch https://hepcloud-git.fnal.gov/ECF-GCO-public/gco_scripts.git /tmp/gco_scripts
 
 cp /tmp/gco_scripts/htcondor/cmslpc-local-conf.py /usr/local/bin/cmslpc-local-conf.py
 
 echo Cleaning up /etc/condor/config.d
-rm -rf /etc/condor/config.d/*
+#rm -rf /etc/condor/config.d/*
 
 echo Replacing various HTCondor commands with CSI/GCO wrappers
 for i in "${REPLACE_CONDOR_CMD[@]}"
@@ -59,8 +59,8 @@ cp /tmp/gco_scripts/htcondor/cmslpc-condor-submit.py /usr/local/bin/condor_submi
 
 rm -rf /tmp/gco_scripts
 
-echo Creating scratch directory for condor wrappers
-mkdir -p /storage/local/data1/condor/config.d
+echo Setting scratch directory for condor wrappers
+export FERMIHTC_LOCALCONF_OVERRIDE='/tmp'
 echo Done
 
 echo ===== Putting configuration files in place
@@ -68,21 +68,20 @@ echo Cleaning up /etc/condor/config.d
 rm -rf /etc/condor/config.d/00*
 
 mkdir -p /etc/condor/certs
-git clone https://hepcloud-git.fnal.gov/ECF-GCO-public/htcondor-config-files.git /tmp/htcondor-config-files
-cp /tmp/htcondor-config-files/service_configs/*cmslpc_interactive* /etc/condor/config.d/
-cp /tmp/htcondor-config-files/mapfiles/cmslpc.condor_mapfile /etc/condor/certs/condor_mapfile
+git clone -q -b eaf_jupyter --single-branch https://hepcloud-git.fnal.gov/ECF-GCO-public/htcondor-config-files.git /tmp/htcondor-config-files
+cp /tmp/htcondor-config-files/service_configs/01_${NB_HTCPOOL}_jupyter_interactive /etc/condor/config.d/
+#cp /tmp/htcondor-config-files/mapfiles/cmslpc.condor_mapfile /etc/condor/certs/condor_mapfile
+cp /tmp/htcondor-config-files/mapfiles/${NB_HTCPOOL}.condor_mapfile /etc/condor/certs/condor_mapfile
 
 rm -rf /tmp/htcondor-config-files
 
 echo ===== Setitng X509 environment
 # Need to set the X509 environment for users
 USCMS_HOME=/uscms/home/${NB_USER}
-echo $USCMS_HOME
-#export X509_USER_PROXY=\$USCMS_HOME/x509up_u\$(/usr/bin/id -u)\n
 #export X509_USER_PROXY="${USCMS_HOME}/x509up_u$(/usr/bin/id -u)"
 export X509_USER_PROXY="${USCMS_HOME}/x509up_u${NB_UID}"
 echo $X509_USER_PROXY
 
 echo ===== Starting a simple condor_master
-$(condor_config_val MASTER)
+#$(condor_config_val MASTER)
 echo Done
