89611aec0fcdc82d1ed1b6e5e7a79cf657d49f5d
macosta <macosta@fnal.gov>
2021-10-05 22:00:30 -0500

    Removing krb5 from init base script

--- base-notebook/init.sh
+++ base-notebook/init.sh
@@ -5,7 +5,3 @@ echo This Jupyter notebook was requested by:
 echo "* Username $NB_USER"
 echo "* UID $NB_UID"
 echo "* GID $NB_GID"
-
-echo ===== Initializing krb5 configuration
-wget https://authentication.fnal.gov/krb5conf/SL7/krb5.conf --output-document=/etc/krb5.conf
-echo "$(awk '{print} /libdefaults/ && !n {print "    udp_preference_limit = 1"; n++}' /etc/krb5.conf)" > /etc/krb5.conf
