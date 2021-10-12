1efdec42345dffa00f3a8cc6d272c07fdc19a9c6
Maria A <mapsacosta@gmail.com>
2021-10-12 11:08:33 -0500

    Update init.sh

--- base-notebook/init.sh
+++ base-notebook/init.sh
@@ -5,9 +5,3 @@ echo This Jupyter notebook was requested by:
 echo "* Username $NB_USER"
 echo "* UID $NB_UID"
 echo "* GID $NB_GID"
-
-cd /usr/local/bin && git clone https://github.com/tskirvin/json-passwd.git
-
-json-fetchpasswdfile --test 
-
-
