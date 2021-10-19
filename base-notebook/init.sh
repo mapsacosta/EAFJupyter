c8763f54e1998e72a1c1c2748d956ca872c93bd9
macosta <macosta@fnal.gov>
2021-10-19 14:25:03 -0500

    Testing new init script

--- base-notebook/init.sh
+++ base-notebook/init.sh
@@ -5,3 +5,7 @@ echo This Jupyter notebook was requested by:
 echo "* Username $NB_USER"
 echo "* UID $NB_UID"
 echo "* GID $NB_GID"
+
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/etc/sssd/passwd
+export NSS_WRAPPER_GROUP=/etc/sssd/group
