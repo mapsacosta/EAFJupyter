4b3646a39b46669223cb4fa22222671d2b6d8a3f
MariaA <macosta@fnal.gov>
2023-02-07 15:18:08 -0600

    Enabling NBsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -11,13 +11,14 @@ version = '0.1.0'
 
 # -- General configuration
 
-#extensions = [
+extensions = [
+    "nbsphinx",
 #    'sphinx.ext.duration',
 #    'sphinx.ext.doctest',
 #    'sphinx.ext.autodoc',
 #    'sphinx.ext.autosummary',
 #    'sphinx.ext.intersphinx',
-#]
+]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
